c6c2abf2baeabdd007c2fef8ce0301a9e5bff403
Ayan <aybarua@gmail.com>
2020-07-05 19:25:43 -0500

    updated some methods

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,16 +1,40 @@
-public class LinkedListDeque<T> {
+public class LinkedListDeque<T>{
     private class TNode {
         public T item;
         public TNode prev;
         public TNode next;
 
-        public TNode(T i, TNode p, TNode n) {
-            item = i;
-            prev = p;
-            next = n;
+        public TNode(T item, TNode prev, TNode next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
         }
     }
-    public TNode sentinel;
     public int size;
-    
+    public TNode sentinel;
+
+    public LinkedListDeque() {
+        size = 0;
+        sentinel = new TNode(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+    }
+
+    public void addFirst(T item) {
+        size++;
+        sentinel.next = new TNode(item, sentinel, sentinel);
+        sentinel.prev = sentinel.next;
+    }
+
+    public void addLast(T item) {
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
 }
